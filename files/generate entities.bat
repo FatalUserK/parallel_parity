2630fee1cfc5d2196cab753d8135125ff77f8747
UserK <111083281+FatalUserK@users.noreply.github.com>
2025-03-21 00:37:15 +0000

    worked more on settings.lua, taking break

---
+++ files/generate entities.bat
@@ -0,0 +1,2 @@
+lua "C:\Program Files (x86)\Steam\steamapps\common\Noita\mods\parallel_parity\files\generate_entities.lua"
+pause
--- settings.lua
+++ settings.lua
@@ -33,6 +33,10 @@ local translation_strings = {
 		en_desc = "Pixel Scenes that are larger than a single chunk (512x512 area)",
         lavalake2 = {
 			en = "Lava Lake",
+            ["ORB.is_local"] = {
+                en = "Spawn Orb",
+                en_desc = "Dictates whether or not the Lava\nLake orb spawns in Parallel Worlds"
+            }
 		},
         skull_in_desert = {
 			en = "Desert Skull",
@@ -71,17 +75,20 @@ local translation_strings = {
 local settings = {
     {
         id = "spliced_pixel_scenes",
+		type = "group",
         items = {
             {
                 id = "lavalake2",
                 value_default = true,
                 scope = MOD_SETTING_SCOPE_NEW_GAME,
-            },
-            {
-                id = "lavalake2.ORB.is_local",
-                value_default = true,
-                requires = { id = "lavalake2", value = true },
-                scope = MOD_SETTING_SCOPE_NEW_GAME,
+                dependents = {
+                    {
+                        id = "ORB.is_local",
+                        value_default = true,
+                        requires = { id = "lavalake2", value = true },
+                        scope = MOD_SETTING_SCOPE_NEW_GAME,
+                    },
+                }
             },
             {
                 id = "skull_in_desert",
@@ -168,16 +175,16 @@ local function update_translations(input_settings, input_translations)
 
 		if setting.items then
 			update_translations(setting.items, input_translations[setting.id])
+        elseif setting.dependents then
+			update_translations(setting.dependents, input_translations[setting.id])
 		end
 	end
 end
 
 function ModSettingsUpdate(init_scope)
 	current_language = languages[GameTextGetTranslatedOrNot("$current_language")]
-    print("A")
 	update_translations()
 
-    print("B")
 	local function set_defaults(setting)
 		if setting.type == "group" then
 			for i, item in ipairs(setting.items) do
@@ -187,6 +194,11 @@ function ModSettingsUpdate(init_scope)
 			if setting.value_default ~= nil then
 				ModSettingSetNextValue(get_setting_id(setting.id), setting.value_default, true)
 			end
+            if setting.dependents then
+                for i, item in ipairs(setting.dependents) do
+                    set_defaults(setting.dependents)
+                end
+            end
 		end
 	end
 	local function save_setting(setting)
@@ -199,6 +211,11 @@ function ModSettingsUpdate(init_scope)
 			if next_value ~= nil then
 				ModSettingSet(get_setting_id(setting.id), next_value)
 			end
+            if setting.dependents then
+                for i, item in ipairs(setting.dependents) do
+                    save_setting(setting.dependents)
+                end
+            end
 		end
 	end
 	for i, setting in ipairs(settings) do
@@ -208,63 +225,33 @@ function ModSettingsUpdate(init_scope)
 	ModSettingSet(get_setting_id("_version"), mod_settings_version)
 end
 
-function ModSettingsGui( gui, in_main_menu )
+
+--mod settings code nabbed from Anvil of Destiny, thanks Horscht o/
+function ModSettingsGui( gui, in_main_menu, _settings, offset)
+    offset = offset or 0
+    _settings = _settings or settings
 	local id = 0
 	local function get_id()
 		id = id + 1
 		return id
 	end
 
-	for i, setting in ipairs(settings) do
+	for i, setting in ipairs(_settings) do
+        offset = offset + (setting.offset or 0)
 		if not setting.requires or (setting.requires and (ModSettingGetNextValue(get_setting_id(setting.requires.id)) == setting.requires.value)) then
 			if setting.type == "group" then
 				GuiOptionsAddForNextWidget(gui, GUI_OPTION.DrawSemiTransparent)
-				GuiText(gui, 0, 0, setting.label)
+				GuiText(gui, offset, 0, setting.label)
 				if setting.description then
 					GuiTooltip(gui, setting.description, "")
 				end
-				GuiLayoutBeginHorizontal(gui, 0, 0)
-				local offset = setting.offset or 0
-	
-				-- Render labels
-				GuiLayoutBeginVertical(gui, 0, 0)
-				for i, setting in ipairs(setting.items) do
-					GuiText(gui, 0, 0, setting.label)
-				end
-				GuiLayoutEnd(gui)
-	
-				-- Render sliders
-				GuiLayoutBeginVertical(gui, 0, 0)
-				for i, setting in ipairs(setting.items) do
-					local next_value = ModSettingGetNextValue(get_setting_id(setting.id))
-					local new_value = GuiSlider(gui, get_id(), 10 + offset, 2, "", next_value, setting.value_min, setting.value_max, setting.value_default, setting.value_display_multiplier, " " or setting.value_display_formatting, 80)
-					GuiLayoutAddVerticalSpacing(gui, 1)
-					if new_value ~= next_value then
-						ModSettingSetNextValue(get_setting_id(setting.id), new_value, false)
-					end
-				end
-				GuiLayoutEnd(gui)
-	
-				-- Render values
-				GuiLayoutBeginVertical(gui, 0, 0)
-				for i, setting in ipairs(setting.items) do
-					local next_value = ModSettingGetNextValue(get_setting_id(setting.id))
-					GuiText(gui, offset + 30, 0, setting.format_fn and setting.format_fn(next_value) or tostring(next_value))
-				end
-				GuiLayoutEnd(gui)
-	
-				GuiLayoutEnd(gui)
-				-- Need to do this because the game doesn't count how many items are in the vertical group
-				for i=2, #setting.items do
-					GuiText(gui, 0, 0, " ")
-				end
-				-- A little margin at the bottom before the next group or items
-				GuiLayoutAddVerticalSpacing(gui, 5)
+
+                ModSettingsGui(gui, in_main_menu, setting.items, offset + 15) --i think recursion just works here
 			else
 				if type(setting.value_default) == "boolean" then
 					local next_value = ModSettingGetNextValue(get_setting_id(setting.id))
-					local text = ("(%s) %s"):format(next_value and "*" or "  ", setting.label)
-					local clicked, right_clicked = GuiButton(gui, 0, 0, text, get_id())
+					local text = ("%s [%s]"):format(setting.label, next_value and "*" or "  ")
+					local clicked, right_clicked = GuiButton(gui, offset, 0, text, get_id())
 					if setting.description then
 						GuiTooltip(gui, setting.description, "")
 					end
@@ -274,55 +261,17 @@ function ModSettingsGui( gui, in_main_menu )
 					if right_clicked then
 						ModSettingSetNextValue(get_setting_id(setting.id), setting.value_default, false)
 					end
-				elseif setting.type == "fine_tuner" then
-					local next_value = ModSettingGetNextValue(get_setting_id(setting.id))
-					local new_value = next_value
-					GuiLayoutBeginHorizontal(gui, 0, 0)
-					GuiText(gui, 0, 0, setting.label .. " ")
-					if setting.description then
-						GuiTooltip(gui, setting.description, "")
-					end
-					local function revert_to_default()
-						new_value = setting.value_default or 0
-					end
-					local left_clicked, right_clicked = GuiButton(gui, get_id(), 0, 0, "[--]")
-					if left_clicked then
-						new_value = new_value - 10
-					elseif right_clicked then
-						revert_to_default()
-					end
-					local left_clicked, right_clicked = GuiButton(gui, get_id(), 0, 0, "[-]")
-					if left_clicked then
-						new_value = new_value - 1
-					elseif right_clicked then
-						revert_to_default()
-					end
-					new_value = math.max(setting.value_min or -999999, new_value)
-					GuiText(gui, 0, 0, (" %s "):format(new_value))
-					local left_clicked, right_clicked = GuiButton(gui, get_id(), 0, 0, "[+]")
-					if left_clicked then
-						new_value = new_value + 1
-					elseif right_clicked then
-						revert_to_default()
-					end
-					local left_clicked, right_clicked = GuiButton(gui, get_id(), 0, 0, "[++]")
-					if left_clicked then
-						new_value = new_value + 10
-					elseif right_clicked then
-						revert_to_default()
-					end
-					GuiLayoutEnd(gui)
-					new_value = math.min(setting.value_max or 999999, new_value)
-					if new_value ~= next_value then
-						ModSettingSetNextValue(get_setting_id(setting.id), new_value, false)
-					end
 				elseif type(setting.value_default) == "number" then
 					local next_value = ModSettingGetNextValue(get_setting_id(setting.id))
-					local new_value = GuiSlider(gui, get_id(), 0, 0, setting.label .. " ", next_value, setting.value_min, setting.value_max, setting.value_default, setting.value_display_multiplier or 1, setting.value_display_formatting or " $0", 80)
+					local new_value = GuiSlider(gui, get_id(), offset, 0, setting.label .. " ", next_value, setting.value_min, setting.value_max, setting.value_default, setting.value_display_multiplier or 1, setting.value_display_formatting or " $0", 80)
 					if new_value ~= next_value then
 						ModSettingSetNextValue(get_setting_id(setting.id), new_value, false)
 					end
 				end
+
+                if setting.dependents then
+                    ModSettingsGui(gui, in_main_menu, setting.dependents, offset + 10)
+                end
 			end
 		end
 	end
